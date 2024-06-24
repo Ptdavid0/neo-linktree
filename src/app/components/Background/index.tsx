55686dfee17085fbb3f218189507d13da53ba8ff
Pedro Antônio David Jorge Gomes Pereira <pedroantonio.dj.pereira@gmail.com>
2024-06-24 20:44:23 -0300

    feat: adding background lava balls

---
+++ src/app/components/Background/index.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import React, { useEffect, useRef, useState } from "react";
+
+export function Background() {
+  const [lavaBalls, setLavaBalls] = useState({
+    circle1: { x: 400, y: 200, xSpeed: 0.2, ySpeed: 0.34 },
+    circle2: { x: 600, y: 400, xSpeed: -0.5, ySpeed: 0.5 },
+    circle3: { x: 800, y: 600, xSpeed: 0.3, ySpeed: 0.6 },
+    circle4: { x: 1000, y: 500, xSpeed: -0.1, ySpeed: 0.6 },
+    circle5: { x: 900, y: 300, xSpeed: -0.2, ySpeed: 0.3 },
+    bounds: {
+      minX: -150,
+      minY: -150,
+      maxX: window.innerWidth + 100,
+      maxY: window.innerHeight + 100,
+    },
+  });
+
+  const updateCirclePosition = (circle: any) => {
+    circle.x += circle.xSpeed;
+    circle.y += circle.ySpeed;
+    if (circle.x < lavaBalls.bounds.minX) {
+      circle.x = lavaBalls.bounds.minX + Math.random() * 10;
+      circle.xSpeed = Math.abs(circle.xSpeed);
+    } else if (circle.x > lavaBalls.bounds.maxX) {
+      circle.x = lavaBalls.bounds.maxX - Math.random() * 10;
+      circle.xSpeed = -Math.abs(circle.xSpeed);
+    }
+    if (circle.y < lavaBalls.bounds.minY) {
+      circle.y = lavaBalls.bounds.minY + Math.random() * 10;
+      circle.ySpeed = Math.abs(circle.ySpeed);
+    } else if (circle.y > lavaBalls.bounds.maxY) {
+      circle.y = lavaBalls.bounds.maxY - Math.random() * 10;
+      circle.ySpeed = -Math.abs(circle.ySpeed);
+    }
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLavaBalls((prevLavaBalls) => {
+        const newLavaBalls = { ...prevLavaBalls };
+        updateCirclePosition(newLavaBalls.circle1);
+        updateCirclePosition(newLavaBalls.circle2);
+        updateCirclePosition(newLavaBalls.circle3);
+        updateCirclePosition(newLavaBalls.circle4);
+        updateCirclePosition(newLavaBalls.circle5);
+        return newLavaBalls;
+      });
+    }, 10);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div id="app" className="fixed inset-0 overflow-hidden bg-black">
+      <div className="metaballs fixed inset-0 -z-10 overflow-hidden">
+        <svg className="metasvg fixed h-[110vh] w-[110vw] top-[-20px] left-[-20px] overflow-hidden">
+          <defs>
+            <filter id="gooify" width="400%" x="-10%" height="400%" y="-150%">
+              <feGaussianBlur
+                in="SourceGraphic"
+                stdDeviation="15"
+                result="blur"
+              />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0
+                        0 1 0 0 0
+                        0 0 1 0 0
+                        0 0 0 25 -10"
+              />
+            </filter>
+
+            <linearGradient id="lavaGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+              <stop offset="0%" stopColor="#9867e2" />
+              <stop offset="50%" stopColor="#ff6eab" />
+              <stop offset="100%" stopColor="#ffd94a" />
+            </linearGradient>
+          </defs>
+
+          <g filter="url(#gooify)">
+            <circle
+              className="blobb glow"
+              fill="url(#lavaGradient)"
+              cx={lavaBalls.circle1.x}
+              cy={lavaBalls.circle1.y}
+              r="170"
+            />
+            <circle
+              className="blobb glow"
+              fill="url(#lavaGradient)"
+              cx={lavaBalls.circle2.x}
+              cy={lavaBalls.circle2.y}
+              r="90"
+            />
+            <circle
+              className="blobb glow"
+              fill="url(#lavaGradient)"
+              cx={lavaBalls.circle3.x}
+              cy={lavaBalls.circle3.y}
+              r="120"
+            />
+            <circle
+              className="blobb glow"
+              fill="url(#lavaGradient)"
+              cx={lavaBalls.circle4.x}
+              cy={lavaBalls.circle4.y}
+              r="140"
+            />
+            <circle
+              className="blobb glow"
+              fill="url(#lavaGradient)"
+              cx={lavaBalls.circle5.x}
+              cy={lavaBalls.circle5.y}
+              r="70"
+            />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
